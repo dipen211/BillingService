4d2a53cf1d9b4ca90da352ce99f5d2e9b697ccfb
dipen.patel <dipen.patel@covrtech.com>
2024-12-15 17:44:23 +0530

    update code for all path allow

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ import productRoute from './routes/product.route.js';
 import cartRouter from './routes/cart.route.js';
 import invoiceRoute from './routes/invoice.route.js';
 import http from 'http';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
 // Create a server object
 const server = http.createServer((req, res) => {
@@ -14,6 +16,9 @@ const server = http.createServer((req, res) => {
     res.end('Welcome to my simple Node.js app!');
 })
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const app = express()
 
 // middleware
@@ -21,17 +26,22 @@ app.use(express.json())
 app.use(urlencoded({ extended: false }))
 
 const corsOptions = {
-    origin: 'https://hiva-sanitrade.onrender.com', // Allow only this origin
+    origin: ['https://hiva-sanitrade.onrender.com',"http://localhost:3000"],// Allow only this origin
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', // Allowed HTTP methods
     credentials: true, // Allow cookies or authorization headers
 };
 
 app.use(cors(corsOptions));
+app.use(express.static(path.join(__dirname, 'build')));
 
 // routes
 app.use("/api/products", productRoute)
 app.use("/api/cart", cartRouter)
 app.use("/api/invoice", invoiceRoute)
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
 
 mongoose.connect('mongodb+srv://dipen211:Dipen%40211@backenddb.te6jm.mongodb.net?retryWrites=true&w=majority&appName=BackendDB')
     .then(() => {
